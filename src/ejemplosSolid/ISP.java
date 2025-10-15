14110d5cd1e4dd3595565daa67d8e5704e8873e1
JosuePach3co <150095207+JosuePach3co@users.noreply.github.com>
2025-10-15 14:36:50 -0500

    Se agrega de nuevo ISP
    No se guardo la clase ISP,  se lo vuelve a agregar.

--- src/ejemplosSolid/ISP.java
+++ src/ejemplosSolid/ISP.java
@@ -1,5 +1,61 @@
 package ejemplosSolid;
 
-public class ISP {
+public interface ISP {
+    
+    public interface ImpresoraMultifuncional {
+        void imprimirDocumento(String documento);
+        void escanearDocumento(String documento);
+        void enviarPorFax(String documento);
+    }
+
+
+    public class ImpresoraAntigua implements ImpresoraMultifuncional{
+        @Override
+        public void imprimirDocumento(String documento) {
+            System.out.println("Imprimiendo: " + documento);
+        }
+
+        @Override
+        public void escanearDocumento(String documento) {
+            System.out.println("Scanneando: " + documento);
+        }
+
+        @Override
+        public void enviarPorFax(String documento) {
+            
+            System.out.println("FAX NO SOPORTADO");
+        }
+
+    }
+
+    //SOLUCION
+
+    public interface IImprimir {
+        void imprimirDocumento(String documento);
+    }
+
+
+    public interface IEscaner {
+        void escanearDocumento(String documento);
+    }
+
+    public interface IFax {
+        void enviarPorFax(String documento);
+    }
+
+    public class ImpresoraAntigua1 implements IImprimir, IEscaner{
+        @Override
+        public void imprimirDocumento(String documento) {
+            System.out.println("Imprimiendo: " + documento);
+        }
+
+        @Override
+        public void escanearDocumento(String documento) {
+            System.out.println("Scanneando: " + documento);
+        }
+
+    }
+
+
 
 }
